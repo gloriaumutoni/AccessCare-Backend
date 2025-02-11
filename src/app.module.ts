32a66d43d12fad426300c7a1a63a9612caf0da8f
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-11 22:32:35 +0200

    chore: create user dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { User } from './users/user.entity';
       entities: [User],
       synchronize: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
