cbbcc7e965ce446e50c48ef3dfed00394cf97721
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-11 21:59:46 +0200

    chore: connect to databse and create user entitie

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sql',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/user.entity.ts
@@ -0,0 +1,17 @@
+import { Options } from '@nestjs/common';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+}
