22dfc1d804523c6ab6ae6f960333a3d46c778fb5
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-11 22:35:25 +0200

    chore: implement user registration endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  register(@Body() body: CreateUserDto) {}
 }
