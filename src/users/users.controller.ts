0d86ba0a127743529a15e44ccde2719b8fc6ba90
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-02-13 20:43:47 +0200

    chore: create users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post('register')
-  register(@Body() body: CreateUserDto) {}
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+    return this.usersService.create(body.name, body.email, hashedPassword);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
+
+  create(name: string, email: string, password: string) {
+    return this.userRepository.save({ name, email, password });
+  }
 }
