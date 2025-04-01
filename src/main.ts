76fdce944afff0b3ea7498ca534375fee5e864f5
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-04-01 23:48:54 +0200

    chore: refactor

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,5 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
