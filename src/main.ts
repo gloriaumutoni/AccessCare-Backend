646e5abedc5bcf59c3f7a8d49212e0142571902c
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-04-02 21:05:04 +0200

    refactor: change origin cors

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'http://127.0.0.1:5173',
-      'http://127.0.0.1:5174',
-      true,
-    ],
-  });
+  app.enableCors();
 
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
