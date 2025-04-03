9123214a79581b1da65eddae5045d1b3d99e2d1a
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-04-04 00:41:55 +0200

    chore: update connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,12 +11,8 @@ import { AppointmentModule } from './appointment/appointment.module';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
+        url: configService.get('DATABASE_URL'),
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
         autoLoadEntities: true,
         synchronize: true,
       }),
