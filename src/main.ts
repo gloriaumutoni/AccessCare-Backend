6fdc6cab051bed3314eea9937b02a78f3f8b2188
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-04-07 01:58:31 +0200

    chore: remove middleware

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,6 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-      res.header(
-        'Access-Control-Allow-Origin',
-        'https://accesscare-fe.netlify.app',
-      );
-      res.header(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PUT, DELETE, OPTIONS',
-      );
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-  });
-
   app.enableCors({
     origin: ['https://accesscare-fe.netlify.app', 'http://localhost:5173'],
     preflightContinue: true,
