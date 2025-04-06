87dc2ea35fdf8bd3d9890ee0d91a5a343dc88313
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-04-07 00:11:21 +0200

    chore: add url origin

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://accesscare-fe.netlify.app',
+    origin: ['https://accesscare-fe.netlify.app', 'http://localhost:5173'],
     preflightContinue: true,
   });
 
