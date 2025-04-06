85cfc6b951ff8670e0adaae984c4e433ff0acef5
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-04-06 23:22:14 +0200

    chore: updatea orgin

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
+    origin: 'accesscare-fe.netlify.app',
     preflightContinue: true,
   });
 
