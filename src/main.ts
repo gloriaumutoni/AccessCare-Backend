90dc74e928744e733201811a1dc85b07f6fe7d26
gloriaumutoni <gloriaumutoni54@gmail.com>
2025-04-07 02:21:37 +0200

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.enableCors({
-    origin: ['https://accesscare-fe.netlify.app', 'http://localhost:5173'],
-    preflightContinue: true,
-  });
+  // app.enableCors({
+  //   origin: ['https://accesscare-fe.netlify.app', 'http://localhost:5173'],
+  //   preflightContinue: true,
+  // });
 
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
